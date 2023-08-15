4afe35d5c45739307a5e4a6757f7e1d54547a5fa
glnro <lauren@informal.systems>
2023-08-15 20:16:04 +0200

    Add benchmark

--- client/main.go
+++ client/main.go
@@ -4,12 +4,90 @@ import (
 	"context"
 	"fmt"
 	cl "github.com/comet/comet-companion/client/client/grpc"
+	cometClient "github.com/cometbft/cometbft/rpc/grpc/client"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"time"
 )
 
 func main() {
+	ctx, ctxCancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer ctxCancel()
+	conn := createGRPCConnection(ctx)
+
+	getVersion(ctx, conn)
+	getBlockResults(ctx, conn)
+	getLatestBlockResults(ctx, conn)
+	getLatestBlock(ctx, conn)
+	getBlock(ctx, conn)
+}
+
+func createGRPCConnection(ctx context.Context) cometClient.Client {
+	addr := "127.0.0.1:5702"
+
+	conn, err := cometClient.New(ctx, addr, cometClient.WithInsecure())
+	if err != nil {
+		fmt.Errorf("failed to dial %s: %w", addr, err)
+	}
+	return conn
+}
+
+func getVersion(ctx context.Context, conn cometClient.Client) {
+	reqStart := time.Now()
+	_, err := conn.GetVersion(ctx)
+	reqEnd := time.Now()
+	if err != nil {
+		fmt.Errorf("failed to retrieve version: %w", err)
+	}
+	elapsed := reqEnd.Sub(reqStart).Milliseconds()
+	fmt.Println(fmt.Sprintf("Get Version response took %v ms", elapsed))
+}
+
+func getBlockResults(ctx context.Context, conn cometClient.Client) {
+	reqStart := time.Now()
+	_, err := conn.GetBlockResults(ctx, 1)
+	reqEnd := time.Now()
+	if err != nil {
+		fmt.Errorf("failed to retrieve block Results: %w", err)
+	}
+	elapsed := reqEnd.Sub(reqStart).Milliseconds()
+	fmt.Println(fmt.Sprintf("Get Block Results took %v ms", elapsed))
+}
+
+func getLatestBlockResults(ctx context.Context, conn cometClient.Client) {
+	reqStart := time.Now()
+	_, err := conn.GetLatestBlockResults(ctx)
+	reqEnd := time.Now()
+	if err != nil {
+		fmt.Errorf("failed to retrieve latest block Results: %w", err)
+	}
+	elapsed := reqEnd.Sub(reqStart).Milliseconds()
+	fmt.Println(fmt.Sprintf("Get Latest Block Results took %v ms", elapsed))
+}
+
+func getLatestBlock(ctx context.Context, conn cometClient.Client) {
+	reqStart := time.Now()
+	_, err := conn.GetLatestBlock(ctx)
+	reqEnd := time.Now()
+	if err != nil {
+		fmt.Errorf("failed to retrieve block Results: %w", err)
+	}
+	elapsed := reqEnd.Sub(reqStart).Milliseconds()
+	fmt.Println(fmt.Sprintf("Get Latest Block took %v ms", elapsed))
+}
+
+func getBlock(ctx context.Context, conn cometClient.Client) {
+	reqStart := time.Now()
+	_, err := conn.GetBlockByHeight(ctx, 1)
+	reqEnd := time.Now()
+	if err != nil {
+		fmt.Errorf("failed to retrieve block Results: %w", err)
+	}
+	elapsed := reqEnd.Sub(reqStart).Milliseconds()
+	fmt.Println(fmt.Sprintf("Get Block took %v ms", elapsed))
+}
+
+func vanillaGrpc() {
 	ctx, ctxCancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer ctxCancel()
 	addr := "127.0.0.1:5702"
--- go.mod
+++ go.mod
@@ -10,12 +10,18 @@ require (
 
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/oasisprotocol/curve25519-voi v0.0.0-20220708102147-0a8a51822cae // indirect
 	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1 // indirect
 	golang.org/x/net v0.12.0 // indirect
@@ -23,4 +29,5 @@ require (
 	golang.org/x/text v0.11.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
